72f4e40b23d7ccbd3f0184b1f1f6c44e16467176
BrachiShkonlik <b0533104431@gmail.com>
2023-07-25 17:07:52 +0300

    goooood

--- db_managment/network_and_devices_CRUD.py
+++ db_managment/network_and_devices_CRUD.py
@@ -51,16 +51,40 @@ async def insert_connections(list_of_connections: List[Connection]):
         raise Exception("Technician not recognized in the system.")
 
 
+async def get_device_by_filter(network_id, the_filter):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM device WHERE network_id = %s"
+            params = [network_id]
+            for key, value in the_filter.items():
+                sql += " AND %s = %s"
+                params.append(key)
+                params.append(value)
+            print(params)
+            print(sql)
+            cursor.execute(sql, params)
+            result = cursor.fetchall()
+            connection.close()
+            return result
+    except Exception:
+        connection.rollback()
+
+# finally:
+# connection.close()
+
+
 async def main():
     # id = await create_network({"client_id": 1, "net_location": "Haifa", "production_date": "2023-07-25"})
     # print("ID:", id)
     # d = Device(network_id=1, mac_address="00:1A:2B:3C:4D:5E", ip_address="192.168.0.1", vendor="Cisco")
     # list_of_devices = [d]
     # await insert_network(list_of_devices)
-    l = [Connection(src=1, dst=1, protocol="tcp/ip")]
-    a = await insert_connections(l)
-    print(a)
+    # l = [Connection(src=1, dst=1, protocol="tcp/ip")]
+    # a = await insert_connections(l)
+    # print(a)
+    filter_conditions = {"vendor": "Cisco"}
+    devices = await get_device_by_filter(1, filter_conditions)
+    print(devices)
 
 
 asyncio.run(main())
-
