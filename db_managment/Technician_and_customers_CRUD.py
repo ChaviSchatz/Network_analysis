069744a43275d68d3276f523eaee21ca6deac59b
BrachiShkonlik <b0533104431@gmail.com>
2023-07-25 11:46:44 +0300

    I finished my functions😊

--- db_managment/Technician_and_customers_CRUD.py
+++ db_managment/Technician_and_customers_CRUD.py
@@ -1,23 +1,37 @@
 from db_connection import connection
+import asyncio
 
 
-async def insert_client(client):
+async def create_client(client):
     try:
-        client_insert = """INSERT INTO tbl_client (
-           fullName)
-           VALUES (%s)"""
-
-        data = [
-        ]
-
-        connection.execute(client_insert, data)
-        connection.commit()
-        print('Record inserted successfully...')
+        with connection.cursor() as cursor:
+            query = """INSERT INTO client (
+               fullName)
+               VALUES (%s)"""
+            data = (client["fullName"])
+            cursor.execute(query, data)
+            connection.commit()
     except:
         connection.rollback()
     connection.close()
 
-def create_technician(technician):
+
+async def update_technician(technician):
+    try:
+        with connection.cursor() as cursor:
+            query = "UPDATE technician SET fullName=%s, hashed_password=%s WHERE id=%s"
+            data = (technician["fullName"], technician["hashed_password"], technician["id"])
+            print("B")
+            cursor.execute(query, data)
+            print("c")
+            connection.commit()
+            print("D")
+    except:
+        connection.rollback()
+    connection.close()
+
+
+async def create_technician(technician):
     try:
         with connection.cursor() as cursor:
             query = """INSERT into technician (fullName,hashed_password)
@@ -29,7 +43,7 @@ def create_technician(technician):
         raise Exception("can't insert technician to db")
 
 
-def update_costumer(client):
+async def update_costumer(client):
     try:
         with connection.cursor() as cursor:
             sql = "UPDATE client SET fullName=%s WHERE id=%s VALUES(%s,%s)"
@@ -40,4 +54,11 @@ def update_costumer(client):
         raise Exception("can't update client to db")
 
 
-update_costumer({"fullName": "Yosef Cohen", "id": 1})
+async def main():
+    # update_costumer({"fullName": "Yosef Cohen", "id": 1})
+    #  await create_client({"fullName": "Yorram"})
+    # await create_technician({"fullName":"Danni", "hashed_password": "$2b$12$.MNAWIkzK6QF11dRVmMxXOzcfxUuRH.Udf7DsQ.27nT0X4FO4qeN."})
+    await update_technician({"fullName":"Ron", "hashed_password": "$2b$12$.MNAWIkzK6QF11dRVmMxXOzcfxUuRH.Udf7DsQ.27nT0X4FO4qeN.", "id":1})
+
+
+asyncio.run(main())
--- db_managment/models/entities.py
+++ db_managment/models/entities.py
@@ -1,27 +1,28 @@
 from pydantic import BaseModel, constr
 from pymysql import Date
+from typing import Union
 
 
 class Client(BaseModel):
-    id: int
+    id = Union[int, None] = None
     fullName: constr(max_length=40)
 
 
 class Technician(BaseModel):
-    id: int
+    id = Union[int, None] = None
     fullName: constr(max_length=40)
     hashed_password: constr(max_length=100)
 
 
 class Network(BaseModel):
-    id: int
+    id = Union[int, None] = None
     client_id: int
     net_location: constr(max_length=100)
     production_date: Date
 
 
 class Device(BaseModel):
-    id: int
+    id = Union[int, None] = None
     network_id: int
     mac_address: constr(min_length=15, max_length=17)
     ip_address: constr(min_length=15, max_length=39)
@@ -29,7 +30,7 @@ class Device(BaseModel):
 
 
 class Connection(BaseModel):
-    id: int
+    id = Union[int, None] = None
     src: int
     dst: int
     protocol: constr(max_length=15)
