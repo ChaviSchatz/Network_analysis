926d9ec254a74b70cc171221fae665aa3e7f028b
YehuditShrg <y0548562311@outlook.co.il>
2023-07-30 13:46:10 +0300

    get devices of network

--- file_mangement/mapping_file.py
+++ file_mangement/mapping_file.py
@@ -1,11 +1,32 @@
-from ..db_managment.models.entities import Device, Connection
 from typing import List
+from scapy.all import rdpcap
+from db_managment.models.entities import Device, Connection
+from mac_vendor_lookup import MacLookup
 
 
-def map_devices(path) -> List[Device]:
+async def map_devices(path, network_id) -> List[Device]:
+    # return list of unique devices that connected to the current network
+    # gets path to the pcap file
+    # gets the network id that this file got and add it to each device
+    try:
+        scapy_cap = rdpcap(path)
+        packets = list(scapy_cap)
+        # devices = List[Device]
+        devices = list()
+        for packet in packets:
+            e = packet["Ether"]
+            mac_address = e.src
+            if not(mac_address in [a.mac_address for a in devices]):
+                vendor = "MacLookup().lookup(mac_address)"
+                device = Device(mac_address=mac_address, ip_address=e.dst, vendor=str(vendor), network_id=network_id)
+                devices.append(device)
+        return devices
+    except Exception:
+        raise Exception("Failed to read the file")
+
+
+def map_connections(path, network_id) -> List[Connection]:
     pass
 
 
-def map_connections(path) -> List[Connection]:
-    # print(12)
-    pass
+print(map_devices(r"C:\Users\user\Downloads\evidence01.pcap", 1))
